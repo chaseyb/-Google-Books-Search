f967e2cc7304bb5b34ea806959de8521c5d4d693
Chase Johnson <Chase.b.johnson@gmail.com>
2020-09-01 21:41:11 -0700

    Updated Api routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,13 @@
+const path = require("path");
 const router = require("express").Router();
-const postRoutes = require("./posts");
+const apiRoutes = require("./api");
 
-// Post routes
-router.use("/posts", postRoutes);
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
