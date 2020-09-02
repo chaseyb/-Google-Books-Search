dd126f7f9a5fdb3f329a68246277495052e3acbf
Chase Johnson <Chase.b.johnson@gmail.com>
2020-09-01 23:27:16 -0700

    Updated book.js

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,7 +1,14 @@
-const router = require("express").Router();
-const postRoutes = require("./posts");
+const router = require('express').Router();
+const booksController = require('../../controllers/booksController');
 
-// Post routes
-router.use("/posts", postRoutes);
+// Matches with "/api/books"
+router.route('/').get(booksController.findAll).post(booksController.create);
+
+// Matches with "/api/books/:id"
+router
+	.route('/:id')
+	.get(booksController.findById)
+	.put(booksController.update)
+	.delete(booksController.remove);
 
 module.exports = router;
